17c773ecd5cb79bf52bf987395267161d5786409
Drew Marsh <drmarsh@microsoft.com>
2018-03-22 15:57:16 -0700

    Added props to IConversation/UserState

--- libraries/Microsoft.Bot.Builder.Core.State/ConversationStateManager.cs
+++ libraries/Microsoft.Bot.Builder.Core.State/ConversationStateManager.cs
@@ -2,12 +2,22 @@
 {
     public interface IConversationStateManager : IStateManager
     {
+        string ChannelId { get; }
+        string ConversationId { get; }
     }
 
     public class ConversationStateManager : StateManager, IConversationStateManager
     {
+        public static string PreferredStateStoreName = "ConversationStateStore";
+
         public ConversationStateManager(string channelId, string conversationId, IStateStore stateStore) : base($"/channels/{channelId}/conversations/{conversationId}", stateStore)
         {
+            ChannelId = channelId ?? throw new System.ArgumentNullException(nameof(channelId));
+            ConversationId = conversationId ?? throw new System.ArgumentNullException(nameof(conversationId));
         }
+
+        public string ChannelId { get; }
+
+        public string ConversationId { get; }
     }
 }
--- libraries/Microsoft.Bot.Builder.Core.State/UserStateManager.cs
+++ libraries/Microsoft.Bot.Builder.Core.State/UserStateManager.cs
@@ -2,12 +2,19 @@
 {
     public interface IUserStateManager : IStateManager
     {
+        string UserId { get; }
     }
 
     public class UserStateManager : StateManager, IUserStateManager
     {
+        public static string PreferredStateStoreName = "UserStateStore";
+
+
         public UserStateManager(string userId, IStateStore stateStore) : base($"/users/{userId}", stateStore)
         {
+            UserId = userId ?? throw new System.ArgumentNullException(nameof(userId));
         }
+
+        public string UserId { get; }
     }
 }
