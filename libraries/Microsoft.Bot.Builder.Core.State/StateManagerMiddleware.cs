7aa25a54877c18789df4ae8dcbec62e04cb329f1
Drew Marsh <drmarsh@microsoft.com>
2018-03-22 14:30:37 -0700

    Initial impl of new state management APIs

---
+++ libraries/Microsoft.Bot.Builder.Core.State/StateManagerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Core.State
+{
+    public sealed class StateManagerAutoLoaderMiddleware : IMiddleware
+    {
+        public StateManagerAutoLoaderMiddleware(bool enableAutoLoad, bool enableAutoSave)
+        {
+            EnableAutoLoad = enableAutoLoad;
+            EnableAutoSave = enableAutoSave;
+        }
+
+        public bool EnableAutoLoad { get; }
+        public bool EnableAutoSave { get; }
+
+        public async Task OnProcessRequest(ITurnContext context, MiddlewareSet.NextDelegate next)
+        {
+            var stateManagerServices = context.Services.GetServices<IStateManager>().ToList();
+
+            if(EnableAutoLoad && stateManagerServices.Count > 0)
+            {
+                await Task.WhenAll(stateManagerServices.Select(sms => sms.Value.Load()));
+            }
+
+            await next();
+
+            if(EnableAutoSave && stateManagerServices.Count > 0)
+            {
+                await Task.WhenAll(stateManagerServices.Select(sms => sms.Value.SaveChanges()));
+            }
+        }
+    }
+}
---
+++ libraries/Microsoft.Bot.Builder.Core.State/StateOptimisticConcurrencyViolation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Microsoft.Bot.Builder.Core.State
+{
+    [Serializable]
+    internal class StateOptimisticConcurrencyViolation : Exception
+    {
+        public StateOptimisticConcurrencyViolation()
+        {
+        }
+
+        public StateOptimisticConcurrencyViolation(string message) : base(message)
+        {
+        }
+
+        public StateOptimisticConcurrencyViolation(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StateOptimisticConcurrencyViolation(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
