d0b65356bc1654990b73bc177c8338b6a3217236
Drew Marsh <drmarsh@microsoft.com>
2018-03-27 11:27:20 -0700

    Initial (non-implemented) FileSystemStateStore
    Adds the initial implementation of the `FileSystemStateStore` and
    brings back its test suite. Right now it's not implemented, so all
    tests are expected to be failing.

---
+++ libraries/Microsoft.Bot.Builder.Core.State/FileSystemStateStore.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Core.State
+{
+    public class FileSystemStateStore : IStateStore
+    {
+        private readonly string _basePath;
+
+        public FileSystemStateStore(string basePath)
+        {
+            _basePath = basePath;
+        }
+
+        public IStateStoreEntry CreateNewStateEntry(string stateNamespace, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(string stateNamespace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(string stateNamespace, IEnumerable<string> keys)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IStateStoreEntry>> Load(string stateNamespace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IStateStoreEntry> Load(string stateNamespace, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IStateStoreEntry>> Load(string stateNamespace, IEnumerable<string> keys)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save(IEnumerable<IStateStoreEntry> stateStoreEntries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- tests/Microsoft.Bot.Builder.Core.Extensions.Tests/FileStorageTests.cs
+++ tests/Microsoft.Bot.Builder.Core.Extensions.Tests/FileStorageTests.cs
@@ -5,6 +5,7 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Builder.Core.State;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Bot.Builder.Core.Extensions.Tests
@@ -17,54 +18,47 @@ namespace Microsoft.Bot.Builder.Core.Extensions.Tests
         [TestCategory("Storage - File")]
         public class Storage_FileTests : StorageBaseTests
         {
-            private IStorage storage;
-            public Storage_FileTests() { }
+            private IStateStore stateStore;
 
             [TestInitialize]
             public void Initialize()
             {
-                string path = Path.Combine(Environment.GetEnvironmentVariable("temp"), "FileStorageTest");
+                string path = Path.Combine(Path.GetTempPath(), "BotFramework/FileSystemStateStoreTests");
                 if (!Directory.Exists(path))
                     Directory.CreateDirectory(path);
                 foreach (var file in Directory.GetFiles(path))
                     File.Delete(file);
-                storage = new FileStorage(path);
+                stateStore = new FileSystemStateStore(path);
             }
 
             [TestMethod]
             public async Task FileStorage_CreateObjectTest()
             {
-                await base._createObjectTest(this.storage);
+                await base._createObjectTest(this.stateStore);
             }
 
             [TestMethod]
             public async Task FileStorage_ReadUnknownTest()
             {
-                await base._readUnknownTest(this.storage);
+                await base._readUnknownTest(this.stateStore);
             }
 
             [TestMethod]
             public async Task FileStorage_UpdateObjectTest()
             {
-                await base._updateObjectTest(this.storage);
+                await base._updateObjectTest(this.stateStore);
             }
 
             [TestMethod]
             public async Task FileStorage_DeleteObjectTest()
             {
-                await base._deleteObjectTest(this.storage);
+                await base._deleteObjectTest(this.stateStore);
             }
 
             [TestMethod]
             public async Task FileStorage_HandleCrazyKeys()
             {
-                await base._handleCrazyKeys(this.storage);
-            }
-
-            [TestMethod]
-            public async Task FileStorage_TypedSerialization()
-            {
-                await base._typedSerialization(this.storage);
+                await base._handleCrazyKeys(this.stateStore);
             }
         }
     }
